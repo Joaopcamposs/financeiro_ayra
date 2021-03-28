0ccd4d92e361b85ad735cc5206bc01d295d6e65d
Joao Pedro Campos <joaopcamposs@outlook.com>
2021-03-28 20:42:13 -0300

    Alteracoes nas classes HomePage e SalePage. Funçao salvar contato e adicionando campo Data ao Helper

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:financeiro_ayra/ui/sale_page.dart';
 import 'package:flutter/material.dart';
+import 'package:financeiro_ayra/helpers/sale_helper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,38 +10,85 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Color colorAyra = const Color(0xFFC69C6D);
 
+  SaleHelper helper = SaleHelper();
+
+  List<Sale> sales = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Financeiro Ayra"),
-        backgroundColor: colorAyra,
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                width: 180,
-                height: 121,
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    image: DecorationImage(
-                        image: AssetImage("images/logoAyra.png"))),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 25),
-                child: Text("Vendas no mês: ")
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: Text("Financeiro Ayra"),
+          backgroundColor: colorAyra,
+          centerTitle: true,
         ),
-      )
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _showSalePage();
+          },
+          child: Icon(Icons.add),
+          backgroundColor: colorAyra,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: 180,
+                  height: 121,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      image: DecorationImage(
+                          image: AssetImage("images/logoAyra.png"))),
+                ),
+                TextField(
+                  enabled: false,
+                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Text(
+                      "Vendas no mês: ",
+                      style:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Text(
+                      "Total de gastos: ",
+                      style:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Text(
+                      "Lucro: ",
+                      style:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Text(
+                      "Ticket Médio: ",
+                      style:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void _showSalePage() {
+    Navigator.push(context, MaterialPageRoute(
+        builder: (context) => SalePage()));
   }
 }
--- lib/ui/sale_page.dart
+++ lib/ui/sale_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:financeiro_ayra/helpers/sale_helper.dart';
+import 'package:intl/intl.dart';
 
 class SalePage extends StatefulWidget {
   @override
@@ -6,70 +8,120 @@ class SalePage extends StatefulWidget {
 }
 
 class _SalePageState extends State<SalePage> {
-
   final _custoController = TextEditingController();
   final _valorController = TextEditingController();
   final _tamanhoController = TextEditingController();
   final _corController = TextEditingController();
   final _refenciaController = TextEditingController();
 
+  SaleHelper helper = SaleHelper();
+  Sale _newSale;
+
   Color colorAyra = const Color(0xFFC69C6D);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Cadastrar Venda"),
+        appBar: AppBar(
+            title: Text("Cadastrar Venda"),
             backgroundColor: colorAyra,
             centerTitle: true),
-        body: Padding(
-          padding: EdgeInsets.all(10),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
           child: Column(
             children: [
               Container(
                 padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                width: 180,
-                height: 121,
+                width: 144,
+                height: 96,
                 decoration: BoxDecoration(
                     shape: BoxShape.rectangle,
                     image: DecorationImage(
-                        image: AssetImage("images/logoAyra.png")
-                    )
-                ),
+                        image: AssetImage("images/logoAyra.png"))),
               ),
-              Padding(padding: EdgeInsets.only(top: 25),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
                 child: TextField(
                   controller: _custoController,
                   decoration: InputDecoration(labelText: "Valor de custo"),
                   keyboardType: TextInputType.number,
-                ),),
-              Padding(padding: EdgeInsets.only(top: 15),
+                  onChanged: (text){
+                    setState(() {
+                      _newSale.custo = text;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
                 child: TextField(
                   controller: _valorController,
                   decoration: InputDecoration(labelText: "Valor de venda"),
                   keyboardType: TextInputType.number,
+                  onChanged: (text){
+                    setState(() {
+                      _newSale.valor = text;
+                    });
+                  },
                 ),
               ),
-              Padding(padding: EdgeInsets.only(top: 15),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
                 child: TextField(
                   controller: _tamanhoController,
                   decoration: InputDecoration(labelText: "Tamanho"),
+                  onChanged: (text){
+                    setState(() {
+                      _newSale.tamanho = text;
+                    });
+                  },
                 ),
               ),
-              Padding(padding: EdgeInsets.only(top: 15),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
                 child: TextField(
                   controller: _corController,
                   decoration: InputDecoration(labelText: "Cor"),
+                  onChanged: (text){
+                    setState(() {
+                      _newSale.color = text;
+                    });
+                  },
                 ),
               ),
-              Padding(padding: EdgeInsets.only(top: 15),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
                 child: TextField(
                   controller: _refenciaController,
                   decoration: InputDecoration(labelText: "Referência"),
+                  onChanged: (text){
+                    setState(() {
+                      _newSale.ref = text;
+                    });
+                  },
                 ),
               ),
+              Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Container(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(colorAyra)),
+                      child: Text(
+                        "Cadastrar venda",
+                        style: TextStyle(color: Colors.white, fontSize: 22),
+                      ),
+                      onPressed: () {
+                        _newSale.data = DateFormat("dd/MM/yyyy").format(DateTime.now());
+                        helper.saveSale(_newSale);
+                        Navigator.pop(context, AlertDialog(content: Text("Venda salva com suceso"),));
+                      },
+                    ),
+                  )),
             ],
           ),
-        )
-    );
+        ));
   }
 }
